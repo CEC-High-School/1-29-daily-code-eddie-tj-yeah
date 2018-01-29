a460bc77d9304e64cdda901d038e97a775e45759
Edwood98 <31854901+Edwood98@users.noreply.github.com>
2018-01-29 08:17:48 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,41 @@
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ALLEGRO_DISPLAY *window = NULL;
+
+	al_init();
+
+	al_init_font_addon();
+	al_init_ttf_addon();
+	window = al_create_display(800, 800);
+	int age;
+	al_clear_to_color(al_map_rgb(100, 255, 100));
+	ALLEGRO_FONT *font = al_load_ttf_font("hp.ttf", 72, 0);
+	al_draw_textf(font, al_map_rgb(255, 100, 100), 400, 364, 1, "What is your age?");
+	al_flip_display();
+
+	cin >> age;
+	al_clear_to_color(al_map_rgb(100, 255, 100));
+	al_draw_textf(font, al_map_rgb(255, 100, 100), 400, 364, 1, "Happy %dth birthday!", age);
+	al_flip_display();
+	al_rest(2);
+	
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+
+
+
+
+
+
+
+
+}
